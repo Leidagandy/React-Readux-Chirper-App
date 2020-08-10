913659a8b6a4af9f7a55924fbbaf24567d7d8ee3
Leida_Gandy <leida.gandy@gmail.com>
2020-08-10 08:33:51 +0100

    Add middleware functions

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import App from './components/App'
 import { createStore } from "redux"
 import { Provider } from "react-redux"
 import reducer from "./reducers"
+import middleware from "./middleware" // go ahead and import the default export coming from
+// the index.js file which is going to be our applied middleware invokation
 
-const store = createStore(reducer) // we call create stroe passing it our root reducer
+const store = createStore(reducer, middleware) // we call create stroe passing it our root reducer
 
 // then we wrap our main app inside the provider component and we pass out store to provider
 
