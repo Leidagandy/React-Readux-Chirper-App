92d9eb37b93443a4f5576e991fff5beaf94aed2a
Leida_Gandy <leida.gandy@gmail.com>
2020-08-10 09:15:35 +0100

    Invoke handleInitialData when the application loads

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { handleInitialData } from "../actions/shared";
 
+// when the component mounts we want to dispatch the invokation of our
+// handleInitialData action creator. In order to get access to dispatch we need to
+// connect our app component
 class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData());
+  }
   render() {
-    return (
-      <div>
-        Starter Code
-      </div>
-    )
+    return <div>Starter Code</div>;
   }
 }
 
-export default App
+export default connect()(App);
+// we dont need anything from the state, we just leave the first invokation blank
+// using connect() function upgrades a component to a container. Containers can read
+// state fro the store and dispatch actions.
