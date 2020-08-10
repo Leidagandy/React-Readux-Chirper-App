c7f33a9f21c68c482f8cec9ac7fe791d08ad9997
Leida_Gandy <leida.gandy@gmail.com>
2020-08-10 19:54:40 +0100

    Add asynchronous action creator to record if a user liked a tweet

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,4 +1,7 @@
+import { saveLikeToggle } from "../utils/api"
+
 export const RECEIVE_TWEETS = "RECEIVE_TWEETS"
+export  const TOGGLE_TWEET = "TOGGLE_TWEET"
 
 // receiveTweets action creator
 
@@ -7,4 +10,28 @@ return {
     type: RECEIVE_TWEETS,
     tweets,
 }
+}
+
+function toggleTweet ({ id, authedUser, hasLiked }) {
+    return {
+        type: TOGGLE_TWEET,
+        id, 
+        authedUser,
+        hasLiked
+    }
+}
+
+export function handleToggleTweet (info) {
+    return (dispatch) => {
+        dispatch(toggleTweet(info))
+
+        return saveLikeToggle(info)
+        .catch((e) => {
+            console.warn("Error in handleToggleTweet: ", e)
+            dispatch(toggleTweet(info))
+            alert("There was an error liking the tweet. Please try again")
+        })
+
+
+    }
 }
