06c258c31b4e087f7d9b2e45e8b124d4d40df9ac
Leida_Gandy <leida.gandy@gmail.com>
2020-08-10 11:26:47 +0100

    Create Dashboard component, display on tweets Id in the Dashboard component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { handleInitialData } from "../actions/shared";
+import Dashboard from "./Dashboard";
 
 // when the component mounts we want to dispatch the invokation of our
 // handleInitialData action creator. In order to get access to dispatch we need to
@@ -9,12 +10,21 @@ class App extends Component {
   componentDidMount() {
     this.props.dispatch(handleInitialData());
   }
+
+  // we only want to render Dashboard once the data from handleInitiaData invokation is finished
+  // if authedUser is true we render null/nothing else we render the dashboard
   render() {
-    return <div>Starter Code</div>;
+    return <div>{this.props.loading === true ? null : <Dashboard />}</div>;
   }
 }
 
-export default connect()(App);
+function mapStateToProps({ authedUser }) {
+  return {
+    loading: authedUser === null, // loading will be true if authedUse is null
+  };
+}
+
+export default connect(mapStateToProps)(App);
 // we dont need anything from the state, we just leave the first invokation blank
 // using connect() function upgrades a component to a container. Containers can read
 // state fro the store and dispatch actions.
