aa97484c82d933318425778786313672515f8a61
Leida_Gandy <leida.gandy@gmail.com>
2020-08-11 15:07:57 +0100

    Add AddTweet action type

--- src/actions/tweets.js
+++ src/actions/tweets.js
@@ -1,35 +1,60 @@
-import { saveLikeToggle } from "../utils/api"
+import { saveLikeToggle, saveTweet } from "../utils/api";
+import { showLoading, hideLoading } from "react-redux-loading";
 
-export const RECEIVE_TWEETS = "RECEIVE_TWEETS"
-export const TOGGLE_TWEET = "TOGGLE_TWEET"
+export const RECEIVE_TWEETS = "RECEIVE_TWEETS";
+export const TOGGLE_TWEET = "TOGGLE_TWEET";
+export const ADD_TWEET = "ADD_TWEET";
 
 // receiveTweets action creator
 
-export function receiveTweets (tweets) { //this will return an object or the action
-return { 
+function addTweet(tweet) {
+  return {
+    type: ADD_TWEET,
+    tweet,
+  };
+}
+
+export function handleAddTweet(text, replyingTo) {
+  return (dispatch, getState) => {
+    const { authedUser } = getState();
+
+    dispatch(showLoading());
+
+    return saveTweet({
+      text,
+      author: authedUser,
+      replyingTo,
+    })
+      .then((tweet) => dispatch(addTweet(twwet)))
+      .then(() => dispatch(hideLoading()));
+  };
+}
+
+export function receiveTweets(tweets) {
+  //this will return an object or the action
+  return {
     type: RECEIVE_TWEETS,
     tweets,
-}
-}
-
-function toggleTweet ({ id, authedUser, hasLiked }) {
-    return {
-        type: TOGGLE_TWEET,
-        id, 
-        authedUser,
-        hasLiked
-    }
+  };
 }
 
-export function handleToggleTweet (info) {
-    return (dispatch) => {
-        dispatch(toggleTweet(info))
+function toggleTweet({ id, authedUser, hasLiked }) {
+  return {
+    type: TOGGLE_TWEET,
+    id,
+    authedUser,
+    hasLiked,
+  };
+}
 
-        return saveLikeToggle(info)
-        .catch((e) => {
-            console.warn("Error in handleToggleTweet: ", e)
-            dispatch(toggleTweet(info))
-            alert("There was an error liking the tweet. Please try again")
-        })
-    }
+export function handleToggleTweet(info) {
+  return (dispatch) => {
+    dispatch(toggleTweet(info));
+
+    return saveLikeToggle(info).catch((e) => {
+      console.warn("Error in handleToggleTweet: ", e);
+      dispatch(toggleTweet(info));
+      alert("There was an error liking the tweet. Please try again");
+    });
+  };
 }
